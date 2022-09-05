ebee31a0392a812d90ae2299042f0fc41677ed98
Derek Sollberger <dsollberger@ucmerced.edu>
2022-09-05 16:32:00 -0700

    lecture 4
    lecture 4

--- WordK/app.R
+++ WordK/app.R
@@ -10,44 +10,54 @@ ui = fluidPage(
 
 set.seed(as.integer(Sys.Date())) #same word for everyone each day?
 #target <- sample(words_common, 1)
-target <- "dread"
+target <- "creek"
 
 server <- function(input, output){
   
+  all_guesses <- character()
   
   output$result <- renderText({
-    # if(nchar(input$guess) != 5){ #an earlier draft
-    if(!(input$guess %in% words_all)){
-      #return("") #returned nothing #an earlier draft
+    user_guess <- stringr::str_to_lower(input$guess)
+    if(!(user_guess %in% words_all)){
       req(FALSE, cancelOutput = TRUE)
     }
-    #paste("You Entered: ", input$guess) #an earlier draft
-    check_report <- check_words(target, input$guess)
-    format_report(check_report)
+    
+    all_guesses <<- c(all_guesses, user_guess)
+    
+    out_str <- vapply(all_guesses, function(guess) {
+      result <- check_words(target, guess)
+      format_report(result)
+    }, character(1))
+    
+    paste(out_str, collapse = "\n")
   }) |>
     bindEvent(input$go)
 }
 
 
 compare_words <- function(target_string, guess_string){
-  # TO DO: more than just check if target == guess
-  # TO DO: return correct letters and/or correct positions
-  
   if(nchar(target_string) != nchar(guess_string)){
     stop("Target and guess strings must have the same length.")
   }
   
   target_vec <- stringr::str_split(target_string, "")[[1]]
   guess_vec  <- stringr::str_split(guess_string, "")[[1]]
-  result_vec <- rep("", 5)
+  remaining_vec <- character(0)
+  result_vec <- rep("not_in_word", 5)
   
-  for(letter in seq_along(target_vec)){
+  for(letter in seq_along(guess_vec)){
     if(guess_vec[letter] == target_vec[letter]){
       result_vec[letter] <- "correct"
-    }else if(guess_vec[letter] %in% target_vec){
+    }else {
+      remaining_vec <- c(remaining_vec, target_vec[letter])
+    }
+  }
+    
+  for(letter in seq_along(guess_vec)){
+    if(guess_vec[letter] != target_vec[letter] && 
+       guess_vec[letter] %in% remaining_vec){
       result_vec[letter] <- "in_word"
-    }else{
-      result_vec[letter] <- "not_in_word"
+      remaining_vec <- remaining_vec[-match(guess_vec[letter], remaining_vec)]
     }
   }
   
@@ -72,14 +82,15 @@ check_words <- function(target_string, guess_string){
 }
 
 format_report <- function(check_report){
-  out_string <- rep("", 5)
+  out_string <- ""
   for(letter in seq_along(check_report$guess_letters)){
+    this_letter <- stringr::str_to_upper(check_report$guess_letters[letter])
     if(check_report$result[letter] == "correct"){
-      out_string[letter] <- paste0("[", check_report$guess_letters[letter], "]")
+      out_string <- paste0(out_string, "[", this_letter, "]")
     }else if(check_report$result[letter] == "in_word"){
-      out_string[letter] <- paste0("(", check_report$guess_letters[letter], ")")
+      out_string <- paste0(out_string, "(", this_letter, ")")
     }else{
-      out_string[letter] <- check_report$guess_letters[letter]
+      out_string <- paste0(out_string, " ", this_letter, " ")
     }
   }
   out_string
